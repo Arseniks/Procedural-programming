7f2f66eec493bcddf327a133ef2341dccb2911a9
Arseniks <48888367+Arseniks@users.noreply.github.com>
2022-09-19 13:37:43 +0300

    Update 4_4.cpp

--- lesson 4/4_4.cpp
+++ lesson 4/4_4.cpp
@@ -3,12 +3,17 @@
 using namespace std;
 
 int main() { 
-    cout << "______________________________________" << endl;
-    for (int i = 0; i < 6; i++) {
-        cout << "********______________________________" << endl;
-    }
-    for (int i = 0; i < 6; i++) {
-        cout << "______________________________________" << endl;
+    for (int i = 0; i < 14; i++) {
+        for (int j = 0; j < 30; j++) {
+            if (i == 0) {
+                cout << "_";
+            } else if (i >= 1 && i <= 6 && j >= 0 && j <= 8) {
+                cout << "*";
+            } else {
+                cout << "_";
+            }
+        }
+        cout << endl;
     }
     return 0;
 }
